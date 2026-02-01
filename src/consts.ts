19cd1b242f49c20b6ec11bd3e7b16a52e7d3b28e
KEee <1470867731@qq.com>
2026-02-01 12:51:44 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -10,9 +10,9 @@ console.log('OWNER:', process.env.NEXT_PUBLIC_GITHUB_OWNER);
  * GitHub 仓库配置
  */
 export const GITHUB_CONFIG = {
-	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'yysuni',
-	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || '2025-blog-public',
+	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'KEee',
+	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || '2025-blog-fork',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
-	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '-',
+	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '2770543',
 	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || 'wudishiduomejimo',
 } as const
